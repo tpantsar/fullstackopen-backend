f762be655cc85e013c82e04fe2db0056b92d44b7
Tomi Pantsar <tomi___00@hotmail.com>
2024-09-17 18:34:24 +0300

    part3 puhelinluettelo backend step6 done

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
-
 let persons = [
   {
     name: "Arto Hellas",
@@ -26,8 +24,30 @@ let persons = [
   },
 ];
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+app.use(express.json());
+app.use(requestLogger);
+app.use(unknownEndpoint);
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
+});
+
+app.get("/info", (request, response) => {
+  response.send(
+    `Phonebook has info for ${persons.length} people <br/> ${new Date()}`
+  );
 });
 
 app.get("/api/persons", (request, response) => {
@@ -45,6 +65,13 @@ app.get("/api/persons/:id", (request, response) => {
   }
 });
 
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
 const generateId = () => {
   const maxId =
     persons.length > 0 ? Math.max(...persons.map((p) => Number(p.id))) : 0;
@@ -54,9 +81,23 @@ const generateId = () => {
 app.post("/api/persons", (request, response) => {
   const body = request.body;
 
-  if (!body.content) {
+  if (!body.name) {
     return response.status(400).json({
-      error: "content missing",
+      error: "name is missing",
+    });
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: "number is missing",
+    });
+  }
+
+  // Check if person name already exists in the phonebook
+  const nameExists = persons.find((person) => person.name === body.name);
+  if (nameExists) {
+    return response.status(400).json({
+      error: "name must be unique",
     });
   }
 
@@ -71,13 +112,6 @@ app.post("/api/persons", (request, response) => {
   console.log(person);
   console.log(request.headers);
   response.json(person);
-});
-
-app.delete("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
 });
 
 const PORT = 3001;
