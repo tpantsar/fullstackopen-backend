683ffb215ca52398bf0260dd9c979f88de2b51ad
Tomi Pantsar <tomi___00@hotmail.com>
2024-09-20 13:46:08 +0300

    use static dist files for deployment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
-const app = express();
 const morgan = require("morgan");
+const app = express();
 
 morgan.token("body", (req) => {
   console.log(req.body);
@@ -16,6 +16,7 @@ app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
+app.use(express.static("dist"));
 
 let persons = [
   {
