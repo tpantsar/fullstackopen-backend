5d7d862c4de39204d8166d1f0c81c77a75fc5ea7
tpantsar <tomi.pantsar@gmail.com>
2024-09-26 15:46:40 +0300

    part3c mongoDB integration done

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-require("dotenv").config();
 const express = require("express");
-const morgan = require("morgan");
-
 const app = express();
+require("dotenv").config();
+
+const morgan = require("morgan");
+app.use(express.static("dist"));
 
 const Person = require("./models/person");
 
@@ -15,11 +16,36 @@ morgan.token("body", (req) => {
   }
 });
 
+const requestLogger = (request, response, next) => {
+  console.log("Method:", request.method);
+  console.log("Path:  ", request.path);
+  console.log("Body:  ", request.body);
+  console.log("---");
+  next();
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
+const cors = require("cors");
+
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
-app.use(express.static("dist"));
+app.use(requestLogger);
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
 
 app.get("/", (request, response) => {
   response.send("<h1>Persons phonebook</h1>");
@@ -39,27 +65,21 @@ app.get("/api/persons", (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = request.params.id;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return response.status(400).json({ error: "invalid ID format" });
-  }
-
-  Person.findById(id)
+app.get("/api/persons/:id", (request, response, next) => {
+  Person.findById(request.params.id)
     .then((person) => {
       if (person) {
         response.json(person);
       } else {
-        response.status(404).json({ error: "person not found" });
+        response.status(404).send({ error: "person not found" });
       }
     })
     .catch((error) => {
-      response.status(500).json({ error: "internal server error" });
+      next(error);
     });
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((person) => {
       if (person) {
@@ -74,7 +94,7 @@ app.delete("/api/persons/:id", (request, response) => {
       }
     })
     .catch((error) => {
-      response.status(500).json({ error: "internal server error" });
+      next(error);
     });
 });
 
@@ -116,11 +136,23 @@ app.post("/api/persons", async (request, response) => {
   }
 });
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
-};
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body;
+
+  const person = {
+    name: body.name,
+    number: body.number,
+  };
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then((updatedPerson) => {
+      response.json(updatedPerson);
+    })
+    .catch((error) => next(error));
+});
 
 app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
